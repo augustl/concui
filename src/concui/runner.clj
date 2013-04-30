ff9a394f88f0ebf889e9a26045671e145333e889
August Lilleaas <august@augustl.com>
2013-04-30 10:54:27 +0200

    Adding licensing information.

--- src/concui/runner.clj
+++ src/concui/runner.clj
@@ -1,3 +1,18 @@
+;; Copyright 2013 August Lilleaas
+;; This file is part of concui.
+;;
+;; concui is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; concui is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with concui. If not, see <http://www.gnu.org/licenses/>.
 (ns concui.runner
   (:require [concui.renderer :as r])
   (:import [org.lwjgl.opengl
